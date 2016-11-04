1d4d54fc6fffc2f8481b17dc48ea8a77660ec697
Nir Benty <nbenty@nvidia.com>
2016-11-04 16:37:42 -0700

    This branch is dedicated for Armor, the little red tank.
    I had to remove vertex colors from the defaultVS because Armor doesn't use them. Apparently, D3D12 excepts the VS input signature to fully match the input-layout. This isn't the same behavior as D3D11, so that sucks. I don't have a solution, so for the sake of Armor I removed vertex colors.
    Nothing crashes, but I get an error that the root signature doesn't match the shaders. Weird, given that I create the root signature based on the program reflection.

--- Framework/Source/Data/VertexAttrib.h
+++ Framework/Source/Data/VertexAttrib.h
@@ -59,7 +59,6 @@ struct VS_IN
     float3 tangent     : TANGENT;
     float3 bitangent   : BITANGENT;
     float2 texC        : TEXCOORD;
-    float3 color       : DIFFUSE_COLOR;
 #ifdef _VERTEX_BLENDING
     float4 boneWeights : BONE_WEIGHTS;
     uint4  boneIds     : BONE_IDS;
@@ -74,7 +73,6 @@ struct VS_OUT
     float3 bitangentW : BITANGENT;
     float2 texC       : TEXCRD;
     float3 posW       : POSW;
-    float3 colorV     : COLOR;
     float4 posH       : SV_POSITION;
 };
 
@@ -101,7 +99,6 @@ VS_OUT defaultVS(VS_IN vIn)
     vOut.posW = mul(worldMat, vIn.pos).xyz;
     vOut.posH = mul(gCam.viewProjMat * worldMat, vIn.pos);
     vOut.texC = vIn.texC;
-    vOut.colorV = vIn.color;
     vOut.normalW = mul((float3x3)worldMat, vIn.normal).xyz;
     vOut.tangentW = mul((float3x3)worldMat, vIn.tangent).xyz;
     vOut.bitangentW = mul((float3x3)worldMat, vIn.bitangent).xyz;
