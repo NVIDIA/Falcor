b79fbdb68b72d2e938ed6e6cb9b32f4b0b8062cb
Nir Benty <nbenty@nvidia.com>
2016-11-14 17:04:30 -0800

    Added an OPTME for SRV/RTV usage when generating mips

--- Framework/Source/API/D3D/D3D12/D3D12Texture.cpp
+++ Framework/Source/API/D3D/D3D12/D3D12Texture.cpp
@@ -322,6 +322,8 @@ namespace Falcor
 
     void Texture::generateMips() const
     {
+        // OPTME D3D12 We are generating SRVs and RTVs for every mip-level of every resource we generate mips for. This is inefficient, especially since we can't release views
+        // We should either reword our descriptor heap to allow us to release views, or optimize genMips() to use pre-allocated views
         if (gGenMips.pFullScreenPass == nullptr)
         {
             gGenMips.pFullScreenPass = FullScreenPass::create("Framework\\GenerateMips.hlsl");
