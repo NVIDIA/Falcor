5ca1c3520de8f431324fe4ea24b72a0ce3a6d222
Nir Benty <nbenty@nvidia.com>
2016-12-21 14:08:59 -0800

    Using xcopy and robocopy to avoid copying files that didn't change

--- Framework/CopyData.bat
+++ Framework/CopyData.bat
@@ -2,13 +2,13 @@
 rem %1==projectDir %2==outputdir
 set local
 
-IF not exist %2\Data\ mkdir %2\Data
-IF exist %1\data\ ( xcopy %1\Data\*.* %2\Data /s /y )
+IF not exist %2\Data\ ( mkdir %2\Data >nul )
+IF exist %1\data\ ( xcopy %1\Data\*.* %2\Data /s /y /d /q >nul)
 
 rem deploy shaders
-IF exist %1\ShadingUtils\ ( xcopy %1\ShadingUtils\*.* %2\Data /s /y )
+IF exist %1\ShadingUtils\ ( xcopy %1\ShadingUtils\*.* %2\Data /s /y /d /q >nul)
 
 rem deploy ray tracing data
-IF exist %1\Raytracing\Data\ ( xcopy %1\Raytracing\Data\*.* %2\Data /s /y )
+IF exist %1\Raytracing\Data\ ( xcopy %1\Raytracing\Data\*.* %2\Data /s /y /d /q >nul)
 
 rem deploy effects
