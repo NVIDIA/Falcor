fcb2328d87368398f79f29faaac01283874a9d43
Conor Lavelle <clavelle@nvidia.com>
2017-01-11 18:19:42 -0800

    Improved automated building, added detecting/logging build errors, and added other misc error handling/logging

--- FalcorTest/BuildFalcorTest.bat
+++ FalcorTest/BuildFalcorTest.bat
@@ -1,29 +1,48 @@
 @echo off
+setlocal ENABLEDELAYEDEXPANSION 
 
-if "%1"=="release" (
-    SET config="released3d12"
-    goto findVS
-    ) 
-if "%1"=="debug" (
-    SET config="debugd3d12"
-    goto findVS
-    )
-echo Usage: BuildFalcorTest.bat ^<release^|debug^>
+set "action="
+if "%1"=="clean" set action=clean
+if "%1"=="build" set action=build
+if "%1"=="rebuild" set action=rebuild
+if not defined action goto usage
 
-exit /B 1
+set "config="
+if "%2"=="release" set config=released3d12
+if "%2"=="debug" set config=debugd3d12
+if not defined config goto usage
+
+goto findVS
 
 :findVS
-if defined VS140COMNTOOLS goto build
+if defined VS140COMNTOOLS goto act
 echo Can't find environment variable "VS140COMNTOOLS". Make sure Visual Studio 2015 is installed correctly.
+exit /B 1
 
-:build
-setlocal
-echo Starting (%config%) build of %2
-call "%VS140COMNTOOLS%\..\IDE\devenv.exe" FalcorTest.sln /rebuild %config% /project %2
-if not %errorlevel%==0 goto buildFailed 
-echo Build Succeeded
+:act
+set project=%3
+set errFileSuffix=_%2%_BuildFailLog.txt
+if defined project (
+    set errFile="%project%%errFileSuffix%"
+    echo Starting %action% of %config% config of project %project%. If fail, log will be written to !errFile!
+    call "%VS140COMNTOOLS%\..\IDE\devenv.com" FalcorTest.sln /%action% %config% /project %project% > !errFile!
+) else (
+    set errFile="FalcorTestSolution%errFileSuffix%"
+    echo Starting %action% of %config% config of entire solution. If fail, log will be written to !errFile!
+    call "%VS140COMNTOOLS%\..\IDE\devenv.com" FalcorTest.sln /%action% %config% > !errFile!
+)
+if not %errorlevel%==0 (
+    goto buildFailed 
+)else (
+    del !errFile!
+)
+
 exit /B 0
 
 :buildFailed
 echo Build Failed
+exit /B 1
+
+:usage
+echo Usage: BuildFalcorTest.bat ^<clean^|build^|rebuild^> ^<release^|debug^> [projectName(entire sln if omitted)]
 exit /B 1
--- FalcorTest/RunAllTests.py
+++ FalcorTest/RunAllTests.py
@@ -3,7 +3,9 @@ import shutil
 import time
 import os
 from xml.dom import minidom
+from xml.parsers.expat import ExpatError
 import argparse
+import sys
 
 testListFile = 'C:\\Users\\clavelle\\Desktop\\FalcorGitHub\\FalcorTest\\TestList.txt'
 buildBatchFile = 'BuildFalcorTest.bat'
@@ -25,11 +27,44 @@ class TestResults(object):
         self.Crashed += other.Crashed
 
 resultList = []
+skippedList = []
 resultSummary = TestResults()
 
-def buildTest(testName, config):
-    subprocess.call([buildBatchFile, config, testName])
+def overwriteMove(filename, newLocation):
+    shutil.copy(filename, newLocation)
+    os.remove(filename)
 
+def logTestSkip(testName, reason):
+    global skippedList
+    skippedList.append((testName, reason))
+
+#args should be action(build, rebuild, clean), config(debug, release), and optionally project
+#if no project given, performs action on entire solution
+def callBatchFile(batchArgs):
+    numArgs = len(batchArgs)
+    if numArgs == 2 or numArgs == 3:
+        batchArgs.insert(0, buildBatchFile)
+        try:
+            return subprocess.call(batchArgs)
+        except WindowsError, info:
+            print 'Error calling batch file. Exception: ', info
+            return 1
+    else:
+        print 'Incorrect batch file call, found ' + string(numArgs) + ' in arg list :' + batchArgs.tostring()
+        return 1
+
+def buildFail(fatal, testName):
+    buildFailLog = testName + "_BuildFailLog.txt"
+    overwriteMove(buildFailLog, resultsFolder)
+    if fatal:
+        print 'Fatal error, failed to build ' + testName
+        sys.exit(1)
+    else:
+        logTestSkip(testName, 'Build Failure')
+
+#crash handling needs to be improved, rn entire test just registers a single 
+#crash, needs to be able to recover and run not crashing tests, probably gonna
+#require some improvements to testbase
 def addCrash(testName):
     global resultList
     global resultSummary
@@ -44,24 +79,31 @@ def processTestResult(testName):
     global resultSummary
     resultFile = testName + "_TestingLog.xml" 
     if os.path.isfile(resultFile):
-        xmlDoc = minidom.parse(resultFile)
+        try:
+            xmlDoc = minidom.parse(resultFile)
+        except ExpatError:
+            logTestSkip(testName, resultFile + ' was found, but is not correct xml')
+            overwriteMove(resultFile, resultsFolder)
+            return
         summary = xmlDoc.getElementsByTagName('Summary')
-        #It makes sense to have this check but it isn't crash friendly, 
-        #I think this will stop the entire testing routine if it can't
-        #figure out a particular test result file
         if len(summary) != 1:
             print 'Error parsing ' + resultFile + ' found no summary or multiple summaries'
-            sys.exit(1)
+            logTestSkip(testName, resultFile + ' was found and is XML, but is improperly formatted')
+            overwriteMove(resultFile, resultsFolder)
+            return
         newResult = TestResults()
+        newResult.Name = testName
         newResult.Total = int(summary[0].attributes['TotalTests'].value)
         newResult.Passed = int(summary[0].attributes['PassedTests'].value)
         newResult.Failed = int(summary[0].attributes['FailedTests'].value)
         resultList.append((testName, newResult))
         resultSummary.add(newResult)
-        shutil.copy(resultFile, resultsFolder)
-        os.remove(resultFile)
+        overwriteMove(resultFile, resultsFolder)
+    else:
+        print 'Unable to find result file ' + resultFile
+        logTestSkip(testName, 'Unable to find result file ' + resultFile)
 
-def readTestList(buildTests):
+def readTestList(buildTests, cleanTests):
     testList = open(testListFile)
     for line in testList.readlines():
         #strip off newline if there is one 
@@ -71,48 +113,53 @@ def readTestList(buildTests):
         spaceIndex = line.find(' ');
         if spaceIndex == -1:
             print 'Skipping improperly formatted test request: ' + line
+            logTestSkip(line, 'Improperly formatted test request')
             continue
 
         testName = line[:spaceIndex]
         configName = line[spaceIndex + 1:]
-        #this would be a good place to eventually check exe exists
         if configName == 'debug' or configName == 'release':
             if buildTests:
-                buildTest(testName, configName)
+                cleanTests
+                #returns 1 on fail
+                if callBatchFile(['build', configName, testName]):
+                    buildFail(False, testName + "_" + configName)
+                    continue
             runTest(testName, configName)
         else:
             print 'Skipping test \"' + testName + '\" Unrecognized desired config \"' + configName + '\"'
+            logTestSkip(testName + "_" + configName, 'Unrecognized config ' + configName)
+
 def runTest(testName, configName):
     if configName == 'debug':
         testPath = debugFolder + testName + ".exe"
     elif configName == 'release':
         testPath = releaseFolder + testName + ".exe"
+
     try:
         if os.path.exists(testPath):
             subprocess.check_call([testPath])
             processTestResult(testName + '_' + configName)
         else:
             print 'Skipping test \"' + testName + '\" Unable to find ' + testPath + ' to run'
+            logTestSkip(testName + '_' + configName, 'Unable to find ' + testPath)
     except subprocess.CalledProcessError:
         addCrash(testName)
 
-def TestResultToHTML(name, result):
-    html = ''
-    html += '<tr>'
-    html += '<td>' + name + '</td>\n'
+def testResultToHTML(result):
+    html = '<tr>'
+    html += '<td>' + result.Name + '</td>\n'
     html += '<td>' + str(result.Total) + '</td>\n'
     html += '<td>' + str(result.Passed) + '</td>\n'
     html += '<td>' + str(result.Failed) + '</td>\n'
     html += '<td>' + str(result.Crashed) + '</td>\n'
-    html += '</tr>'
+    html += '</tr>\n'
     return html
 
-def outputHTML(openSummary):
-    html = ''
+def getTestResultsTable():
     html = '<table style="width:100%" border="1">\n'
     html += '<tr>\n'
-    html += '<th/>\n'
-    html += '<th colspan=\'5\'>TestResults</th>\n'
+    html += '<th colspan=\'5\'>Test Results</th>\n'
     html += '</tr>\n'
     html += '<tr>\n'
     html += '<th>Test</th>\n'
@@ -120,10 +167,37 @@ def outputHTML(openSummary):
     html += '<th>Passed</th>\n'
     html += '<th>Failed</th>\n'
     html += '<th>Crashed</th>\n'
-    html += TestResultToHTML('Summary', resultSummary)
+    resultSummary.Name = 'Summary'
+    html += testResultToHTML(resultSummary)
     for name, result in resultList:
-        html += TestResultToHTML(name, result)
+        html += testResultToHTML(result)
+    html += '</table>\n'
+    return html
+
+def skipToHTML(name, reason):
+    html = '<tr>\n'
+    html += '<td>' + name + '</td>\n'
+    html += '<td>' + reason + '</td>\n'
+    html += '</tr>\n'
+    return html
+
+def getSkipsTable():
+    html = '<table style="width:100%" border="1">\n'
+    html += '<tr>\n'
+    html += '<th colspan=\'2\'>Skipped Tests</th>'
+    html += '</tr><tr>'
+    html += '<th>Test</th>\n'
+    html += '<th>Reason for Skip</th>\n'
+    html += '</tr>\n'
+    for name, reason in skippedList:
+        html += skipToHTML(name, reason)
     html += '</table>'
+    return html
+
+def outputHTML(openSummary):
+    html = getTestResultsTable()
+    html += '<br><br>'
+    html += getSkipsTable()
     date = time.strftime("%m-%d-%y")
     resultSummaryName = resultsFolder + '\\TestSummary_' + date  + '.html'
     outfile = open(resultSummaryName, 'w')
@@ -136,19 +210,33 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-nb', '--nobuild', action='store_true', help='run without rebuilding Falcor and test apps')
     parser.add_argument('-ss', '--showsummary', action='store_true', help='opens testing summary upon completion')
+    #bto only skips building falcor and falcor test if theyre already build, if theyre not, it'll still build them
     parser.add_argument('-bto', '--buildtestsonly', action='store_true', help='builds test apps, but not falcor. Overriden by nobuild')
     args = parser.parse_args()
 
     if not os.path.isdir(resultsFolder):
         os.makedirs(resultsFolder)
+
+    cleanTests = False
+    if not args.nobuild and not args.buildtestsonly:
+        callBatchFile(['clean', 'debug'])
+        callBatchFile(['clean', 'release'])
+    #only clean individual tests if entire soln not already cleaned and want to build tests
+    elif args.buildtestsonly:
+        cleanTests = True
  
     if not args.nobuild and not args.buildtestsonly:
-        buildTest('Falcor', 'debug')
-        buildTest('Falcor', 'release')
-        buildTest('FalcorTest', 'debug')
-        buildTest('FalcorTest', 'release')
+        #returns 1 on fail
+        if callBatchFile(['build', 'debug', 'Falcor']):
+            buildFail(True, 'Falcor_debug')
+        if callBatchFile(['build', 'release', 'Falcor']):
+            buildFail(True, 'Falcor_release')
+        if callBatchFile(['build', 'debug', 'FalcorTest']):
+            buildFail(True, 'FalcorTest_debug')
+        if callBatchFile(['build', 'release', 'FalcorTest']):
+            buildFail(True, 'FalcorTest_release')
 
-    readTestList(not args.nobuild)
+    readTestList(not args.nobuild, cleanTests)
     outputHTML(args.showsummary)
 
 if __name__ == '__main__':
