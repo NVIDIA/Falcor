3f701cea20ffb47685275442beca8bbc35fc2797
Conor Lavelle <clavelle@nvidia.com>
2017-01-24 14:14:20 -0800

    Added Command Line Arg Parsing and System Test Arg Handling

---
+++ Framework/Source/ArgList.cpp
@@ -0,0 +1,111 @@
+#include "ArgList.h"
+#include "Framework.h"
+#include <sstream>
+
+namespace Falcor
+{
+
+    void ArgList::parseCommandLine(const std::string& cmdLine)
+    {
+        std::stringstream args(cmdLine);
+        std::string token;
+        std::string currentArg;
+        while (std::getline(args, token, ' '))
+        {
+            if (token.find('-') != std::string::npos)
+            {
+                currentArg = token.substr(1);
+                addArg(currentArg);
+            }
+            else if(!token.empty())
+            {
+                addArg(currentArg, token);
+            }
+        }
+    }
+
+    void ArgList::addArg(const std::string& arg)
+    {
+        mMap.insert(std::make_pair(arg, std::vector<Arg>()));
+    }
+
+    void ArgList::addArg(const std::string& key, Arg arg)
+    {
+        mMap[key].push_back(arg);
+    }
+
+    bool ArgList::argExists(const std::string& arg)
+    {
+        return mMap.find(arg) != mMap.end();
+    }
+
+    std::vector<ArgList::Arg> ArgList::getValues(const std::string& key)
+    {
+        return mMap[key];
+    }
+
+    const ArgList::Arg& ArgList::operator[](const std::string& key) const
+    {
+        assert(mMap.at(key).size() == 1);
+        return mMap.at(key)[0];
+    }
+
+    int32_t ArgList::Arg::asInt()
+    {
+        try
+        {
+            return std::stoi(mValue);
+        }
+        catch (std::invalid_argument& e)
+        {
+            logWarning("Unable to convert " + mValue + " to int. Exception: " + e.what());
+            return -1;
+        }
+        catch (std::out_of_range& e)
+        {
+            logWarning("Unable to convert " + mValue + " to int. Exception: " + e.what());
+            return -1;
+        }
+    }
+
+    uint32_t ArgList::Arg::asUint()
+    {
+        try
+        {
+            return std::stoul(mValue);
+        }
+        catch (std::invalid_argument& e)
+        {
+            logWarning("Unable to convert " + mValue + " to unsigned. Exception: " + e.what());
+            return -1;
+        }
+        catch (std::out_of_range& e)
+        {
+            logWarning("Unable to convert " + mValue + " to unsigned. Exception: " + e.what());
+            return -1;
+        }
+    }
+
+    float ArgList::Arg::asFloat()
+    {
+        try
+        {
+            return std::stof(mValue);
+        }
+        catch (std::invalid_argument& e)
+        {
+            logWarning("Unable to convert " + mValue + " to float. Exception: " + e.what());
+            return -1;
+        }
+        catch (std::out_of_range& e)
+        {
+            logWarning("Unable to convert " + mValue + " to float. Exception: " + e.what());
+            return -1;
+        }
+    }
+
+    std::string ArgList::Arg::asString()
+    {
+        return mValue;
+    }
+}
