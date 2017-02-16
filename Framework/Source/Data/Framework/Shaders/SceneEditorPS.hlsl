707bdab676ab7201f44e7f27f7448bea16002b6e
Kai-Hwa Yao <kyao@nvidia.com>
2017-02-15 18:21:40 -0800

    Fixed include filename

--- Framework/Source/Data/Framework/Shaders/SceneEditorPS.hlsl
+++ Framework/Source/Data/Framework/Shaders/SceneEditorPS.hlsl
@@ -25,7 +25,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ***************************************************************************/
-#include "EditorCommon.hlsli"
+#include "SceneEditorCommon.hlsli"
 
 #ifndef PICKING
 cbuffer ConstColorCB : register(b0)
