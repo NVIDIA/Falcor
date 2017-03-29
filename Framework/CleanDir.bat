f100df45ab96601976f1ee2d676c991766f581c7
Conor Lavelle <clavelle@nvidia.com>
2017-03-28 17:03:02 -0700

    Only Clean if dir exists

--- Framework/CleanDir.bat
+++ Framework/CleanDir.bat
@@ -2,6 +2,7 @@ rem %1==dirToDelete
 ECHO Cleaning %1
 @echo off
 
+IF EXIST %1 (
 pushd %1
 attrib /s /d -r > nul
 rem set tlogs to read only so they wont be deleted
@@ -9,3 +10,4 @@ attrib /s /d *.tlog +r > nul
 del. /s /q > nul
 attrib /s /d -r > nul
 popd
+)
