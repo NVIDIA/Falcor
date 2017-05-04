b55d37272dd5bc26d64addbc9feadbb44bd368d5
Tim Foley <tfoley@nvidia.com>
2017-05-04 13:15:26 -0700

    Remove use of `#expect`
    This isn't a standard preprocessor feature, so we won't force Spire to support it right away.
    If there is high demand this can be added as a feature request for the Spire rewriter, and prioritized appropriately.

--- Framework/Source/Data/Effects/GaussianBlur.ps.hlsl
+++ Framework/Source/Data/Effects/GaussianBlur.ps.hlsl
@@ -25,7 +25,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ***************************************************************************/
-#expect _KERNEL_WIDTH
+//#expect _KERNEL_WIDTH
 
 #ifdef _USE_TEX2D_ARRAY
     Texture2DArray gSrcTex;
