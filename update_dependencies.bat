bf288b3618bff3cfaef4b7d9228bdb0ea942b2c2
nbenty <nbenty@nvidia.com>
2017-07-25 14:45:45 -0700

    `update_depencencies` will return an error if the `packman` command fails

--- update_dependencies.bat
+++ update_dependencies.bat
@@ -1,2 +1,3 @@
-@if not exist %~dp0Framework\Externals mkdir %~dp0Framework\Externals 
-@call "%~dp0packman\packman.cmd" pull "%~dp0dependencies.xml"
+if not exist %~dp0Framework\Externals mkdir %~dp0Framework\Externals 
+call "%~dp0packman\packman.cmd " pull "%~dp0dependencies.xml"
+if errorlevel 1 exit 1
