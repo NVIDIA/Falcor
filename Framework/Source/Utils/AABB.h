28e5569f3eeee34376d26cf8c58f2562dbfe6282
Kai-Hwa Yao <kyao@nvidia.com>
2017-07-24 19:19:32 -0700

    Documentation for AABB

--- Framework/Source/Utils/AABB.h
+++ Framework/Source/Utils/AABB.h
@@ -32,16 +32,24 @@
 
 namespace Falcor
 {
+    /** An Axis-Aligned Bounding Box
+    */
     struct BoundingBox
     {
-        glm::vec3 center;
-        glm::vec3 extent;
+        glm::vec3 center; ///< Center position of the bounding box
+        glm::vec3 extent; ///< Half length of each side. Essentially the coordinates to the max corner relative to the center.
 
+        /** Checks whether two bounding boxes are equivalent in position and size
+        */
         bool operator==(const BoundingBox& other)
         {
             return (other.center == center) && (other.extent == extent);
         }
 
+        /** Calculates the bounding box transformed by a matrix
+            \param[in] mat Transform matrix
+            \return Bounding box after transformation
+        */
         BoundingBox transform(const glm::mat4& mat) const
         {
             glm::vec3 min = center - extent;
@@ -69,22 +77,35 @@ namespace Falcor
             return BoundingBox::fromMinMax(newMin, newMax);
         }
 
+        /** Gets the minimum position of the bounding box
+            \return Minimum position
+        */
         glm::vec3 getMinPos() const
         {
             return center - extent;
         }
 
+        /** Gets the maximum position of the bounding box
+            \return Maximum position
+        */
         glm::vec3 getMaxPos() const
         {
             return center + extent;
         }
 
+        /** Gets the size of each dimension of the bounding box.
+            \return X,Y and Z lengths of the bounding box
+        */
         glm::vec3 getSize() const
         {
-            return extent*2.0f;
+            return extent * 2.0f;
         }
 
-
+        /** Construct a bounding box from a minimum and maximum point.
+            \param[in] min Minimum point
+            \param[in] max Maximum point
+            \return A bounding box
+        */
         static BoundingBox fromMinMax(const glm::vec3& min, const glm::vec3& max)
         {
             BoundingBox box;
@@ -93,10 +114,14 @@ namespace Falcor
             return box;
         }
 
+        /** Constructs a bounding box from the union of two other bounding boxes.
+            \param[in] bb0 First bounding box
+            \param[in] bb1 Second bounding box
+            \return A bounding box
+        */
         static BoundingBox fromUnion(const BoundingBox& bb0, const BoundingBox& bb1)
         {
-
-            return BoundingBox::fromMinMax( min(bb0.getMinPos(), bb1.getMinPos()), max(bb0.getMaxPos(), bb1.getMaxPos()) );
+            return BoundingBox::fromMinMax(min(bb0.getMinPos(), bb1.getMinPos()), max(bb0.getMaxPos(), bb1.getMaxPos()));
         }
     };
 }
